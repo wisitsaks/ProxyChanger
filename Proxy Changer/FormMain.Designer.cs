5ad9af422132f53c7fac0d904f3b85f5684600ca
Wisitsak.San <wisitsak.san@neu.ac.th>
2021-04-01 14:05:21 +0700

    Initial Version

---
+++ Proxy Changer/FormMain.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace MeaningSolution___Proxy_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIconMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelSetting = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewProxy = new System.Windows.Forms.DataGridView();
+            this.ColumnNetworkName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnProxyIPAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnProxyPort = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStripMain.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProxy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIconMain
+            // 
+            this.notifyIconMain.ContextMenuStrip = this.contextMenuStripMain;
+            this.notifyIconMain.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconMain.Icon")));
+            this.notifyIconMain.Text = "Proxy Changer";
+            this.notifyIconMain.Visible = true;
+            // 
+            // contextMenuStripMain
+            // 
+            this.contextMenuStripMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStripMain.Name = "contextMenuStripMain";
+            this.contextMenuStripMain.Size = new System.Drawing.Size(126, 58);
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(125, 24);
+            this.settingToolStripMenuItem.Text = "Setting";
+            this.settingToolStripMenuItem.Click += new System.EventHandler(this.settingToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(122, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(125, 24);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // labelSetting
+            // 
+            this.labelSetting.BackColor = System.Drawing.Color.Crimson;
+            this.labelSetting.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelSetting.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSetting.ForeColor = System.Drawing.Color.White;
+            this.labelSetting.Location = new System.Drawing.Point(0, 0);
+            this.labelSetting.Name = "labelSetting";
+            this.labelSetting.Size = new System.Drawing.Size(680, 31);
+            this.labelSetting.TabIndex = 1;
+            this.labelSetting.Text = "Setting";
+            this.labelSetting.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonUpdate.BackColor = System.Drawing.Color.LawnGreen;
+            this.buttonUpdate.Location = new System.Drawing.Point(15, 265);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 31);
+            this.buttonUpdate.TabIndex = 2;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.BackColor = System.Drawing.Color.Silver;
+            this.buttonCancel.Location = new System.Drawing.Point(593, 265);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 31);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridViewProxy);
+            this.groupBox1.Location = new System.Drawing.Point(12, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(894, 225);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // dataGridViewProxy
+            // 
+            this.dataGridViewProxy.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridViewProxy.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProxy.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnNetworkName,
+            this.ColumnProxyIPAddress,
+            this.ColumnProxyPort});
+            this.dataGridViewProxy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewProxy.Location = new System.Drawing.Point(3, 18);
+            this.dataGridViewProxy.Name = "dataGridViewProxy";
+            this.dataGridViewProxy.RowTemplate.Height = 24;
+            this.dataGridViewProxy.Size = new System.Drawing.Size(888, 204);
+            this.dataGridViewProxy.TabIndex = 0;
+            // 
+            // ColumnNetworkName
+            // 
+            this.ColumnNetworkName.DataPropertyName = "NetworkName";
+            this.ColumnNetworkName.HeaderText = "Network Name";
+            this.ColumnNetworkName.Name = "ColumnNetworkName";
+            this.ColumnNetworkName.Width = 129;
+            // 
+            // ColumnProxyIPAddress
+            // 
+            this.ColumnProxyIPAddress.DataPropertyName = "ProxyIPAddress";
+            this.ColumnProxyIPAddress.HeaderText = "Proxy IP Address";
+            this.ColumnProxyIPAddress.Name = "ColumnProxyIPAddress";
+            this.ColumnProxyIPAddress.Width = 132;
+            // 
+            // ColumnProxyPort
+            // 
+            this.ColumnProxyPort.DataPropertyName = "ProxyPort";
+            this.ColumnProxyPort.HeaderText = "Proxy Port";
+            this.ColumnProxyPort.Name = "ColumnProxyPort";
+            this.ColumnProxyPort.Width = 94;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 308);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.labelSetting);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Proxy Changer - Setting";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStripMain.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProxy)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconMain;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Label labelSetting;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridViewProxy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNetworkName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnProxyIPAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnProxyPort;
+    }
+}
+
---
+++ Proxy Changer/FormMain.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.IO;
+
+namespace MeaningSolution___Proxy_Changer
+{
+
+    public partial class Form1 : Form
+    {
+        private DataSet ds = new DataSet();
+        private DataTable proxyInfo = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            NetworkChange.NetworkAddressChanged += new
+            NetworkAddressChangedEventHandler(AddressChangedCallback);
+
+            proxyInfo.Columns.Add("NetworkName");
+            //proxyInfo.Columns.Add("NetworkType");
+            proxyInfo.Columns.Add("ProxyIPAddress");
+            proxyInfo.Columns.Add("ProxyPort");
+            ds.Tables.Add(proxyInfo);
+            if (File.Exists("proxy.xml")) ds.ReadXml("proxy.xml");
+            dataGridViewProxy.DataSource = proxyInfo;
+            
+            notifyIconMain.Text = showConnectedId();
+        }
+
+        void AddressChangedCallback(object sender, EventArgs e)
+        {
+            Boolean isFound = false;
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+                foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+                    if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && ni.OperationalStatus == OperationalStatus.Up)
+                    {
+                        Invoke(new Action(() =>
+                        {
+                            notifyIconMain.Text = showConnectedId();
+                        }));
+
+                        isFound = true;
+                        break;
+                    }
+
+            if (!isFound)
+            {
+                Invoke(new Action(() =>
+                {
+                    notifyIconMain.Text = showConnectedId();
+                }));
+            }
+
+        }
+
+        // Show SSID and Signal Strength
+        string showConnectedId()
+        {
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                FileName = "netsh.exe",
+                Arguments = "wlan show interfaces",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+            };
+
+            Process p = Process.Start(startInfo);
+            string s = p.StandardOutput.ReadToEnd();
+            string s1 = "N/A";
+            p.WaitForExit();
+
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+
+            var isFound = false;
+            if (s.Contains("SSID"))
+            {
+                s1 = s.Substring(s.IndexOf("SSID"));
+                s1 = s1.Substring(s1.IndexOf(":"));
+                s1 = s1.Substring(2, s1.IndexOf("\n")).Trim();
+                
+                proxyInfo.Rows.OfType<DataRow>().ToList().ForEach(r =>
+                {
+                    if (s1.Contains(r["NetworkName"].ToString()) && !isFound)
+                    {
+                        registry.SetValue("ProxyEnable", 1);
+                        registry.SetValue("ProxyServer", r["ProxyIPAddress"] + ":" + r["ProxyPort"]);
+                        s1 += " " + r["ProxyIPAddress"] + ":" + r["ProxyPort"];
+                        isFound = true;
+                    }
+                });
+
+
+            }
+
+            if (!isFound)
+            {
+                registry.SetValue("ProxyEnable", 0);
+                registry.SetValue("ProxyServer", "");
+            }
+
+            return "Proxy Changer : " + s1;
+        }
+
+        private void settingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.TopMost = true;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            ds.WriteXml("proxy.xml");
+            this.Hide();
+        }
+    }
+}
